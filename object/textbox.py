1992360294db0fa06543ed96fa021432cc8ba955
wjykk <no@no>
2015-03-06 12:50:23 -0700

    Attempt to remove limit from textboxes

--- object/textbox.py
+++ object/textbox.py
@@ -3,7 +3,7 @@ import pygame, assets, string
 class Textbox:
 	focused = None
 
-	def __init__(self, screen, x, y, w, limit, text = ""):
+	def __init__(self, screen, x, y, w, limit = 0, text = ""):
 		self.font = pygame.font.SysFont(assets.font, 16)
 	
 		self.screen = screen
@@ -21,12 +21,19 @@ class Textbox:
 		if Textbox.focused != self and event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos): Textbox.focused = self
 		elif Textbox.focused == self and event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_BACKSPACE: self.text = self.text[:-1] 
-			elif event.key >= 32 and event.key < 256 and chr(event.key) in string.printable and len(self.text) <= self.limit: self.text += chr(event.key)
+			elif event.key >= 32 and event.key < 256 and chr(event.key) in string.printable and (self.limit == 0 or len(self.text) <= self.limit): self.text += chr(event.key)
 			
 	def logic(self):
 		self.text_surf = self.font.render(self.text + ("_" if Textbox.focused == self and self.blinking else ""), True, (255,0,0))
 		self.text_rect = self.text_surf.get_rect()
 		self.text_rect.topleft = (self.rect.x+2, self.rect.y+2)
+		
+		if self.text_rect.width > self.rect.width:
+			self.text_rect.right = self.rect.right
+			self.text_rect.size = self.rect.size
+			s = pygame.Surface((self.rect.width-4, self.rect.height-4))
+			s.blit(self.text_surf, self.text_rect, (self.text_rect.width-self.rect.width, self.text_rect.height-self.rect.height, self.rect.width, self.rect.height))
+			self.text_surf = s
 	
 		if Textbox.focused == self:
 			self.blink_time += 1
--- screen/options.py
+++ screen/options.py
@@ -34,8 +34,8 @@ class OptionsScreen:
 		self.footer_rect = self.footer.get_rect()
 		self.footer_rect.midbottom = (screen.get_width()/2, screen.get_height()-10)
 		
-		self.player_x_box = Textbox(screen, 20, self.player_x_rect.bottom + 20, screen.get_width()/2 - 40, 11)
-		self.player_o_box = Textbox(screen, screen.get_width()/2 + 20, self.player_o_rect.bottom + 20, screen.get_width()/2 - 40, 11)
+		self.player_x_box = Textbox(screen, 20, self.player_x_rect.bottom + 20, screen.get_width()/2 - 40)
+		self.player_o_box = Textbox(screen, screen.get_width()/2 + 20, self.player_o_rect.bottom + 20, screen.get_width()/2 - 40)
 		
 		types = ((HumanPlayer, HumanPlayer.get_type()), (RandomPlayer, RandomPlayer.get_type()), (BlockingPlayer, BlockingPlayer.get_type()), (SmartPlayer, SmartPlayer.get_type()), (jon, jon.get_type()))
 		self.player_x_type = RadioGroup(screen, 20, self.player_x_rect.bottom + 70, types[0][0], True, *types)
